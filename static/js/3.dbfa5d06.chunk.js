9c050d9860015b9c9bb12f015c773f2587f6456d
Vitaminka2021 <vitalina_88@i.ua>
2021-01-17 16:50:44 +0200

    Updates

--- static/js/3.dbfa5d06.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpnew-life-shop"]=this["webpackJsonpnew-life-shop"]||[]).push([[3],{137:function(e,n,s){"use strict";s.r(n);var t=s(3);s(0);n.default=function(){return Object(t.jsx)("footer",{children:Object(t.jsx)("p",{children:"MainPage"})})}}}]);
-//# sourceMappingURL=3.dbfa5d06.chunk.js.map
